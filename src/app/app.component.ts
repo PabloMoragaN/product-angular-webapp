c5aca62044589e57d0095a663ef18277831e972d
Pablo <p.moraga.navas@gmail.com>
2019-03-13 09:59:03 +0100

    Working toggable Nav/SideBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public title='Virtualizen';
+  public toggled;
+
+
+
+  constructor(){
+    this.toggled=true;
+  }
+
+
+  toggleTopBar(){
+    this.toggled=true;
+  }
+
+  toggleSideBar(){
+    this.toggled=false;
+  }
 
 }
 
